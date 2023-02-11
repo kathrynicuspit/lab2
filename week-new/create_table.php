ad54eaaccfd62280046c327cf9fbcbc697379c05
Kathryn Ann Icuspit <kbicuspit@student.apc.edu.ph>
2023-02-11 23:40:10 +0800

    comment

--- week-new/create_table.php
+++ week-new/create_table.php
@@ -17,8 +17,8 @@ id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(30) NOT NULL,
 email VARCHAR(30) NOT NULL,
 website VARCHAR(50),
-comment VARCHAR(255),
-);
+comment VARCHAR(255)
+)";
 
 if (mysqli_query($conn, $sql)) {
   echo "Table kbicuspit_myguests created successfully";
